b83babe1f9a4b25ee35dcac9635615b95867371b
Filip Hlavac <filip.hlavac01@gmail.com>
2021-11-29 12:13:36 +0100

    Use cors & mysql

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
+// provides Express middleware to enable CORS with various options
+const cors = require('cors');
+
 const app = express();
 
+const corsOptions = {
+  origin: 'http://localhost:8081',
+};
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const categories = require('./routes/categories');
 
+// apply routes
 app.use('/categories', categories);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./index');
 
-app.listen(3000, (err) => {
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (err) => {
   if (err) throw err;
-  console.log('Server running in http://127.0.0.1:3000');
+  console.log(`Server running in http://127.0.0.1:${PORT}`);
 });
